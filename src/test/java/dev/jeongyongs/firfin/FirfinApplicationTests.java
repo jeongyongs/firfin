523ffef15d041ae1c3d3b4b895d6e74e76e80491
Jeongyong Lee <hua9378@gmail.com>
2024-08-13 20:43:09 +0900

    chore: 프로젝트 정리

--- src/test/java/dev/jeongyongs/firfin/FirfinApplicationTests.java
+++ src/test/java/dev/jeongyongs/firfin/FirfinApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FirfinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
