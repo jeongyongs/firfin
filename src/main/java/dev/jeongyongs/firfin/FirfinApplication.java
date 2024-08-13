82083d3859792dcb5302b006365885ec746a246f
Jeongyong Lee <hua9378@gmail.com>
2024-08-13 20:09:21 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/dev/jeongyongs/firfin/FirfinApplication.java
@@ -0,0 +1,13 @@
+package dev.jeongyongs.firfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirfinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jeongyongs/firfin/FirfinApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeongyongs.firfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
