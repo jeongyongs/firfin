5ef9a4b040d04f1fdc5189a79a00d387236a530a
Jeongyong Lee <hua9378@gmail.com>
2024-08-16 14:40:12 +0900

    feat: 중복 요청 응답

--- src/main/java/dev/jeongyongs/firfin/controller/GeneralControllerAdvice.java
+++ src/main/java/dev/jeongyongs/firfin/controller/GeneralControllerAdvice.java
@@ -4,7 +4,9 @@ import dev.jeongyongs.firfin.domain.ErrorCode;
 import dev.jeongyongs.firfin.dto.ErrorResponse;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionTimedOutException;
@@ -32,6 +34,17 @@ public class GeneralControllerAdvice {
         ErrorResponse body = new ErrorResponse(ErrorCode.INVALID_REQUEST, message, LocalDateTime.now());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body(body);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleException(ConstraintViolationException ex) {
+        if (!"23505".equals(ex.getSQLState())) {
+            throw ex;
+        }
+        ErrorResponse body = new ErrorResponse(ErrorCode.DUPLICATED, "중복 요청", LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                              .body(body);
     }
 }
