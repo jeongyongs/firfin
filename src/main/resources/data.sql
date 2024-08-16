823fa78aacaf5ce199eb9127c0b3c367deb3d2d1
Jeongyong Lee <hua9378@gmail.com>
2024-08-16 17:02:50 +0900

    chore: SQL 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,8 +32,8 @@ CREATE TABLE paybacks
     update_at      DATETIME NULL
 );
 
-INSERT INTO users
-VALUES (1, 0, 10000, 0, 300000, 50000, 20000, 100000);
+INSERT INTO users(user_status, money, point, money_limit, per_payment_limit, daily_payment_limit, monthly_payment_limit)
+VALUES (0, 10000, 0, 300000, 50000, 20000, 100000);
 
-INSERT INTO payments
-VALUES (1, 1, 0, X'123e4567e89b12d3a456426614174000', 123, 10000, NOW(), null);
+INSERT INTO payments(user_id, payment_status, transaction_id, merchant_id, amount, create_at, update_at)
+VALUES (1, 0, X'123e4567e89b12d3a456426614174000', 123, 10000, NOW(), null);
