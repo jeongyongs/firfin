664b8f99066875bc0d7080f92888d63c8e6f6e11
Jeongyong Lee <hua9378@gmail.com>
2024-09-12 16:01:24 +0900

    refactor: sonarqube 정적분석
    - 사용하지 않는 의존성 제거
    - jacoco code coverage 사용
    - 스프링 기본 테스트 내용 추가

--- src/main/java/dev/jeongyongs/firfin/repository/PaybackRepository.java
+++ src/main/java/dev/jeongyongs/firfin/repository/PaybackRepository.java
@@ -1,8 +1,6 @@
 package dev.jeongyongs.firfin.repository;
 
 import dev.jeongyongs.firfin.domain.Payback;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaybackRepository extends JpaRepository<Payback, Long> {
--- src/test/java/dev/jeongyongs/firfin/FirfinApplicationTests.java
+++ src/test/java/dev/jeongyongs/firfin/FirfinApplicationTests.java
@@ -1,12 +1,20 @@
 package dev.jeongyongs.firfin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class FirfinApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
 }
